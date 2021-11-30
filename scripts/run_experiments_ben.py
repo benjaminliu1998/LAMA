bd34e8b507ac13dee595bf5fcbb09cca329337a0
Benjamin Liu <benja180@mit.edu>
2021-11-29 21:11:35 -0500

    updated requirements

--- scripts/run_experiments_ben.py
+++ scripts/run_experiments_ben.py
@@ -56,13 +56,15 @@ from collections import defaultdict
 
 LMs = []
 for seed in range(0, 5):
-  for ckpt in ['0', '20', '60', '100', '140', '200', '400', '700', '1000', '1500', '1800', '2000']:
+  # for ckpt in ['0', '20', '60', '100', '140', '200', '400', '700', '1000', '1500', '1800', '2000']:
+  for ckpt in ['60', '100', '140', '200', '400', '700', '1000', '1500', '1800', '2000']:
     LMs.append({
                 "lm": "bert",
                 "label": "multiberts-seed_"+str(seed)+"-step_"+ckpt+"k",
                 "models_names": ["bert"],
                 "bert_model_name": "google/multiberts-seed_"+str(seed)+"-step_"+ckpt+"k",
-                "bert_model_dir": "pre-trained_language_models/bert/multiberts-seed_"+str(seed)+"-step_"+ckpt+"k", # changed from None
+                # "bert_model_dir": "pre-trained_language_models/bert/multiberts-seed_"+str(seed)+"-step_"+ckpt+"k", # changed from None
+                "bert_model_dir": "/Volumes/GoogleDrive/My Drive/G2 2021 Fall/AC 297R Capstone/LAMAProbing_Copy/LAMA-main/pre-trained_language_models/bert/multiberts-seed_"+str(seed)+"-step_"+ckpt+"k", # changed from None
                 # "bert_model_dir": None
                 })
 
@@ -221,13 +223,13 @@ if __name__ == "__main__":
     # parameters = get_GoogleRE_parameters()
     # run_all_LMs(parameters)
 
-    print("2. T-REx")
-    parameters = get_TREx_parameters()
-    run_all_LMs(parameters)
-
-    # print("3. ConceptNet")
-    # parameters = get_ConceptNet_parameters()
+    # print("2. T-REx")
+    # parameters = get_TREx_parameters()
     # run_all_LMs(parameters)
+
+    print("3. ConceptNet")
+    parameters = get_ConceptNet_parameters()
+    run_all_LMs(parameters)
 
     # print("4. SQuAD")
     # parameters = get_Squad_parameters()
